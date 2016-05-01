736d5ade77a2715abba6542342fa97d5b9e90245
ReenExe <ipourri@gmail.com>
2016-05-01 15:24:21 +0300

    generate default User

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Vergo\NaissanceBundle\NaissanceBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
         ];
 
