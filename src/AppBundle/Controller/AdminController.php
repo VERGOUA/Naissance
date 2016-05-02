210e45b00c2838868f9e22e435e3ea548a66d286
Golars <titorens@gmail.com>
2016-05-02 14:56:59 +0300

    progress

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -9,6 +9,7 @@ class AdminController extends Controller
     public function indexAction()
     {
 
+
         return $this->render('AppBundle:Admin\base:index.html.twig', array(
             // ...
         ));
