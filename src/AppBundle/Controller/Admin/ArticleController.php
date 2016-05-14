dbdd5c1b4aff07e0aaff851e2d4eed2a5b1e82c4
ReenExe <ipourri@gmail.com>
2016-05-14 17:12:51 +0300

    default article action

---
+++ src/AppBundle/Controller/Admin/ArticleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ArticleController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Admin\article:index.html.twig');
+    }
+}
