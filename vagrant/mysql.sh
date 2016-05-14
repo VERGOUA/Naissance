76801d38925960b8df1561cf2c4a5388cee51365
ReenExe <ipourri@gmail.com>
2016-05-14 16:11:29 +0300

    update vagrant provision

--- vagrant/mysql.sh
+++ vagrant/mysql.sh
@@ -2,4 +2,4 @@
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password 123"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 123"
 sudo aptitude install -q -y -f  mysql-server mysql-client php5-mysql
-mysql -uroot -p123 -e 'CREATE DATABASE `vergo_base` CHARACTER SET `utf8` COLLATE `utf8_bin`;'
+mysql -uroot -p123 -e 'CREATE DATABASE IF NOT EXISTS `vergo_base` CHARACTER SET `utf8` COLLATE `utf8_bin`;'
